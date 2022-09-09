49e64d425b6532905e1acc4e391b4fc44d7fed91
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-09 19:45:24 +0200

    All bug fixed

--- js/utilities.js
+++ js/utilities.js
@@ -2,8 +2,9 @@ function getInputFieldValueById(inputFieldId) {
   const inputField = document.getElementById(inputFieldId);
   const inputFieldValueString = inputField.value;
   const inputFieldValue = parseFloat(inputFieldValueString);
+  inputField.value = "";
   return inputFieldValue;
-  // inputField.value = ""; //** fix it */
+   //** fix it */
 }
 
 function getTextElementValueById(elementId) {
